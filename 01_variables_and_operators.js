c6cd789aef04ef43596d8491ea3c8c65f3a314a0
lucianofrailelopez <lucianofrailee@gmail.com>
2022-01-07 11:38:23 -0300

    01_variables_and_operators.js

---
+++ 01_variables_and_operators.js
@@ -0,0 +1,34 @@
+
+/*  a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la
+       suma de ambos números en una 3er variable.*/
+
+var agua = 5;
+
+var botella = 8;
+
+var total = agua + botella;
+
+console.log(total)
+
+/* b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er
+      variable.*/
+
+
+var _name = "luciano";
+
+var myName = "I am ";
+
+var concatenacion = (myName + _name);
+
+console.log(concatenacion)
+
+/* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras
+      del string) guardando el resultado de la suma en una 3er variable (utilizar length).*/
+
+var saludo = "Hola ";
+
+var todos = "mundo";
+
+var regards = (saludo + todos);
+
+console.log(regards.length)
